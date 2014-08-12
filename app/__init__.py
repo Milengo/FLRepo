1ab7ca4c7a91aa62d1e88dfcb9a8c0911f589716
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2014-08-12 12:38:23 +0200

    create user model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import config
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -10,4 +12,6 @@ def create_app(config_name):
     from .tms import tms as tms_blueprint
     app.register_blueprint(tms_blueprint)
     bootstrap.init_app(app)
+    db.init_app(app)
+    
     return app
