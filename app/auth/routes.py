8a604e829c2108061e2eb571ee08053f13f814b0
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2014-08-12 17:25:29 +0200

    add automatic avatar support

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user, login_required
 from ..models import User
 from . import auth
 from .forms import LoginForm
