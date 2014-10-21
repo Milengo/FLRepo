a401317f81afa5237040ab92ffa586e940eff9e0
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2014-10-21 09:50:43 +0200

    add filter to template for source and target languages

--- application/mod_memoqclient/__init__.py
+++ application/mod_memoqclient/__init__.py
@@ -13,8 +13,13 @@ mod_memoqclient = Blueprint('mod_memoqclient', __name__, template_folder='templa
 def tm_list():
     tm_client = MemoqTMClient(MEMOQ_SERVER_URL)
     tms = tm_client.get_tm_list("","")
-    
-    return render_template('tm_list.html', tms=tms, tm_count= len(tms))
+    src_langs = {}
+    targ_langs = {}
+    for tm_info in tms:
+    	src_langs[tm_info.SourceLanguageCode]=tm_info.SourceLanguageCode
+    	targ_langs[tm_info.TargetLanguageCode]=tm_info.TargetLanguageCode
+
+    return render_template('tm_list.html', tms=tms, tm_count= len(tms), src=src_langs, trg=targ_langs)
 
 @mod_memoqclient.route('/tm_download/<guid>/<name>') 
 def tm_download(guid, name): 
