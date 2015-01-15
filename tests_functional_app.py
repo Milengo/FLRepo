f684bce6295105f2ba7db397004731a2d6f89977
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2015-01-15 12:31:09 +0100

    using chrome webdriver now

--- tests_functional_app.py
+++ tests_functional_app.py
@@ -4,8 +4,12 @@ import unittest
 
 class IndexPageTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
+    def test_title(self):
+        self.browser.get('http://localhost:5000')
+        self.assertIn('Translation', self.browser.title)
+        self.fail('Finish the test')
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
