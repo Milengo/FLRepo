9aad1acaf9c978ab3c2616c0ab1dc6dd9ffeb4e9
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2015-01-21 20:21:40 +0100

    test for upload TM view

--- test_app.py
+++ test_app.py
@@ -1,13 +1,16 @@
 import os
 import app
 import unittest
-import tempfile
+from tempfile import mkstemp
 
 class FLRepoTestCase(unittest.TestCase):
     def setUp(self):
-        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        self.db_fd, app.config['DATABASE'] = mkstemp()
         app.config['TESTING'] = True
         self.app = app.test_client()
+    def test_index_resolution(self):
+        rv = self.app.get('/')
+        assertEqual('Translation Memory' in rv.data)
     def tearDown(self):
         os.close(db_fd)
         os.unlink(app.config['DATABASE'])
--- tests_functional_app.py
+++ tests_functional_app.py
@@ -5,6 +5,7 @@ import unittest
 class FLRepoTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
     def test_basetemplate(self):
@@ -23,12 +24,19 @@ class FLRepoTest(unittest.TestCase):
         #application should be running
         self.browser.get('http://localhost:5000')
         #user clicks on the upload TM link
-        button = self.browser.find_element_by_name('file')
-        self.browser.click(on_element=button)
-        #it redirects to the "/upload" page
-        #there should be a button "choose file"
+        button = self.browser.find_element_by_id('upload')
+        button.click()
+        #it redirects to the "/upload" page with the header : Upload TM
+        self.assertIn("Upload TM", self.browser.find_element_by_tag_name("h1").text)
+        #there should be a button named "choose file"
+        self.assertTrue(self.browser.find_element_by_id('file'))
         #and the list of Translations memories uploaded to the server
+        self.assertTrue(self.browser.find_element_by_id('tms'))
         #clicking on "choose file" should open "select file" dialog
+        upload_file_btn = self.browser.find_element_by_id('file')
+        upload_file_btn.click()
+        self.browser.switchTo().activeElement().sendKeys('c:\Sites\flrepo\FLRepo\media\CON-CC_Working_en2es-mx_201412450_SW.tmx')
+
         #after file is uploaded, the list of TMs is refreshed to have one additional TM added
         #for each TM, a full TM name should be visible and a button named "Read data"
         #at the bottom we have a link that goes back to index page
