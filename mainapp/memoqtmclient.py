c0a6d9f5bce59f77a1454539ec98a555ebca3cbf
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2015-05-07 12:37:19 +0200

    deleted unused functions definitions

--- mainapp/memoqtmclient.py
+++ mainapp/memoqtmclient.py
@@ -11,8 +11,9 @@ class MemoqTMClient():
         """expects url in form 'http://www.memoq.com'"""
 
         self.server_url = "".join(
-            [server_url,
-            ':8080/memoqservices/tm?singleWsdl'])
+            [
+                server_url,
+                ':8080/memoqservices/tm?singleWsdl'])
         self.tm_service = Client(url=self.server_url)
 
     def get_tm_list(self, source="", target=""):
@@ -28,7 +29,8 @@ class MemoqTMClient():
 
         self.fileh = codecs.open(filename, 'w', encoding='UTF16')
         while 1:
-            self.chunk = self.tm_service.service.GetNextTMXChunk(self.session_id)
+            self.chunk = self.tm_service.service.GetNextTMXChunk(
+                    self.session_id)
             if self.chunk is None:
                 break
 
@@ -39,54 +41,3 @@ class MemoqTMClient():
 
     def create(self, params):
         raise NotImplemented
-
-
-def get_next_chunk(fileh, session_id, tm_service, loop):
-
-    chunk = tm_service.service.GetNextTMXChunk(session_id)
-    if chunk:
-        fileh.write(chunk)
-        loop.call_later(30,
-        functools.partial(
-            get_next_chunk,
-            fileh,
-            session_id,
-            tm_service,
-            loop))
-    else:
-        fileh.close()
-        loop.call_later(
-            2,
-            functools.partial(
-                tm_service.service.EndChunkedTMXExport,
-                session_id))
-
-
-def download_tm(tm_guid):
-
-    tm_service = Client(url="MemoqServer.com/memoqservices/tm?singleWsdl")
-
-    session_id = tm_service.service.BeginChunkedTMXExport(tm_guid)
-    fileh = codecs.open(
-        "6af51589-2c72-4036-bb7c-0bbeed1c96bb.tmx",
-        'w',
-        encoding='UTF16')
-
-    while 1:
-        chunk = tm_service.service.GetNextTMXChunk(session_id)
-        if chunk is None:
-            break
-
-        as_str = str(base64.b64decode(chunk), encoding='utf-16')
-        print(chunk.__doc__)
-
-        fileh.write(as_str)
-    tm_service.service.EndChunkedTMXExport(session_id)
-
-
-def main():
-
-    download_tm("6af51589-2c72-4036-bb7c-0bbeed1c96bb")
-
-if __name__ == '__main__':
-    main()
